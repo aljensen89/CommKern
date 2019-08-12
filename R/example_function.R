c86027aff00fea1e0736594db7bf3146209540ca
AJensen <alexandria.jensen@cuanschutz.edu>
2019-08-12 10:41:39 -0600

    initial commit with example function

---
+++ R/example_function.R
@@ -0,0 +1,16 @@
+#' Example Function
+#' 
+#' Description of the Example Function.  
+#' 
+#' Details....  (can be many paragraphs)
+#' 
+#' @param x a numeric value
+#' @param ymat a matrix
+#' @param ... not currentl used
+#' 
+#' @return the scalar product of x by ymat
+#' 
+#' @export
+example_function <- function(x, ymat, ...) {
+  x * ymat / 2
+}
