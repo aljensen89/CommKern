13a895f5b21f112310fa4a1c76c7188785a1206f
AJensen <alexandria.jensen@cuanschutz.edu>
2019-09-12 16:10:31 -0600

    Added init_lookup function

---
+++ R/init_lookup.R
@@ -0,0 +1,20 @@
+#' Initialize Lookup
+#' 
+#' Description of the initialize lookup function.  
+#' 
+#' From the pottsmodel_2 text file, translating the initialize_lookup function
+#' 
+#' @param kT
+#' @param gamma
+#' 
+#' @return ???
+#'
+#' @examples 
+#' 
+#' example_function(2, matrix(c(1, 2, 3, 4), ncol = 2))
+#'   
+#' @export
+
+init_lookup <- function(kT, gamma){
+  
+}
