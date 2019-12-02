8c8037b97b4bd7ca47202c1a3b098de0481a63bc
AJensen <alexandria.jensen@cuanschutz.edu>
2019-12-02 12:45:26 -0700

    Continued working on find_comm_from_start function

--- R/find_comm_from_start.R
+++ R/find_comm_from_start.R
@@ -46,6 +46,185 @@ find_comm_from_start <- function(gamma,prob,nodename,result,cohesion,
   kir <- 0.0 #double
   w <- 0.0 #double
   
+  community_marker <- 5 #long
+  to_do_marker <- 10 #long
+  
+  inner_links <- 0.0 #double
+  outer_links <- 0.0 #double
+  aff_r <- 0.0 #double
+  aff_s <- 0.0 #double
+  
+  #IGRAPH_UNUSED(prob)
+  
+  #to_do=new DLList<NNode*>
+  #community=new DLList<NNode*>
+  
+  #Find the node in the network
+  #n_cur=iter.First(net->node_list)
+  
+  while(!found & !iter.End()){
+    if(pracma::strcmp(n_cur->Get_Name(), nodename) == 0){
+      start_node <- n_cur
+      found <- TRUE
+      start_node <- Set_Affinity(0.0)
+      community <- Push(start_node)
+      start_node <- Set_Marker(community_marker)
+      Ks <- Get_Weight(start_node)
+      Kr <- Get_Weight(total_degree_sum - start_node)
+    }
+    #n_cur=iter.Next()
+  }
+  
+  if(!found){
+    #delete to_do
+    #delete community
+    -1
+  }
+  
+  #Initialize the to do list and community with the neighbors of start node
+  neighbor <- iter.First(Get_Neighbors(start_node))
+  while(!iter.End()){
+    #Now add at the second neighbors to the to_do list
+    neighbor <- iter2.First(Get_Neighbors(node))
+    while(!iter2.End()){
+      if(Get_Marker(neighbor) != community_marker & Get_Marker(neighbor) != to_do_marker){
+        to_do <- Push(neighbor)
+        neighbor <- Set_Marker(to_do_marker)
+      }
+      #neighbor=iter2.Next
+    }
+    #node=iter.Next()
+  }
+  
+  #Repeat, as long as we are still adding nodes to the community
+  add <- TRUE
+  remove <- TRUE
+  while(add | remove){
+    #Calculate the affinity changes of all nodes for adding every node in the to_do
+    #list to the community
+    
+    max_delta_aff <- 0.0
+    max_aff_node <- NA
+    add <- FALSE
+    node <- iter.First(to_do)
+    while(!iter.End()){
+      degree <- Get_Weight(node)
+      kis <- 0.0
+      kir <- 0.0
+      
+      #For every of the neighbors, check, count the links to the community
+      #l_cur=l_iter.First(node->Get_Links())
+      
+      while(!l_iter.End()){
+        w <- Get_Weight(l_cur)
+        if(node==Get_Start(l_cur)){
+          n_cur <- Get_End(l_cur)
+        }
+        else {
+          n_cur <- Get_Start(l_cur)
+        }
+        if(Get_Marker(n_cur) == community_marker){
+          kis <- kis+w #the weight/number of links to the community
+        }
+        else {
+          kir <- kir+w #the weight/number of links to the rest of the network
+        }
+        #l_cur=l_iter.Next()
+      }
+      
+      aff_r <- kir-gamma/total_degree_sum*(Kr-degree)*degree
+      aff_s <- kis-gamma/total_degree_sum*Ks_degree
+      delta_aff_add <- aff_r-aff_s
+      
+      if(delta_aff <= max_delta_aff){
+        node <- Set_Affinity(aff_s)
+        max_delta_aff <- delta_aff_add
+        max_aff_node <- node
+        add <- TRUE
+      }
+    }
+    
+    #calculate the affinity changes for removing every single node from community
+    inner_links <- 0.0
+    outer_links <- 0.0
+    remove <- FALSE
+    #node=iter.First(community)
+    while(!iter.End()){
+      degree <- Get_Weight(node)
+      kis <- 0.0
+      kir <- 0.0
+      
+      #For every one of the neighbors, check, count the links to the community
+      #l_cur=l_iter.First(node->Get_Links())
+      
+      while(!l_iter.End()){
+        w <- Get_Weight(l_cur)
+        
+        if(node == Get_Start(l_cur)){
+          n_cur <- Get_End(l_cur)
+        }
+        else{
+          n_cur <- Get_Start(l_cur)
+        }
+        
+        if(Get_Marker(n_cur)==community_marker){
+          kis <- kis+w
+          inner_links <- inner_links+w #summing all w gives twice the number of inner links/weights
+        }
+        else{
+          kir <- kir+w
+          outer_links <- outer_links+w
+        }
+        #l_cur=l_iter.Next()
+      }
+      
+      aff_r <- kit-gamma/total_degree_sum*Kr*degree
+      aff_s <- kis-gamma/total_degree_sum*(Ks-degree)*degree
+      delta_aff_rem <- aff_s-aff_r
+      node<-Set_Affinity(aff_s)
+      #We should not remove the nodes we have just added
+      
+      if(delta_aff_rem < max_delta_aff){
+        max_delta_aff <- delta_aff_rem
+        max_aff_node <- node
+        remove <- TRUE
+        add <- FALSE
+      }
+      
+      #node=iter.Next()
+    }
+    inner_links <- inner_links*0.5
+    
+    #Now check whether we want to remove or add a node
+    if(add){
+      #Add the node of maximum affinity to the community
+      community <- Push(max_aff_node)
+      max_aff_node <- Set_Marker(community_marker)
+      
+      #Delete node from to_do
+      #to_do->fDelete(max_aff_node)
+      
+      #Update the sum of degrees in the community
+      
+      Ks <- Ks+Get_Weight(max_aff_node)
+      Kr <- Kr-Get_Weight(max_aff_node)
+      
+      #Now add all neighbors of this node not already in the to_do list or in the community
+      #neighbor=iter.First(max_aff_node->Get_Neighbors())
+      
+      while(!iter.End()){
+        if(Get_Marker(neighbor) != community_marker & Get_Marker(neighbor) != to_do_marker){
+          to_do <- Push(neighbor)
+          neighbor <- Set_Marker(to_do_marker)
+        }
+        #neighbor=iter.Next()
+      }
+    }
+    
+    if(remove){
+      
+    }
+  }
   
   
 }
