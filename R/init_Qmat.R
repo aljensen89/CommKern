22d2d9ecb839d8e2fb6cee126dd4da8955d6138a
AJensen <alexandria.jensen@cuanschutz.edu>
2020-02-27 09:28:55 -0700

    More updates and notes to init_Qmat function

--- R/init_Qmat.R
+++ R/init_Qmat.R
@@ -4,7 +4,7 @@
 #' 
 #' From the pottsmodel_2 text file, translating the initialize_Qmatrix function. From the
 #' documentation in the C++ code: Q denotes the modularity of the network. This function
-#' calculates it initially. In the event of a sping changing its state, it only needs
+#' calculates it initially. In the event of a spin changing its state, it only needs
 #' updating. Note that Qmatrix and Qa are only counting! The normalization by num_of_links
 #' is done later.
 #' 
@@ -17,11 +17,12 @@
 #' @export
 
 init_Qmat <- function(){
-  #DLList_Iter<NLink*> l_iter
-  #NLink *l_cur
+  #DLList_Iter<NLink*> l_iter; ##Creating an iteration using the NLink constructor, function holding edge-based info
+  #NLink *l_cur; ##The current link being examined
   
-  num_of_links <- length(link_list)
+  num_of_links <- length(link_list) #num_of_links=net->link_list->Size();
   
+  #Initialize with zeros
   for (i in 0:q){
     Qa[i] <- 0.0
     for (j in i:q){
@@ -30,16 +31,19 @@ init_Qmat <- function(){
     }
   }
   
-  l_cur <- l_iter[i] #also calling the links list here; using an l_iter.First call
+  #Go over all links and make corresponding entries in Q matrix
+  #An edge connecting state i with state j will get and entry in Q_ij and Q_ji
   
-  while (!l_iter[q]){
-    i <- l_cur #also calling a Get_ClusterIndex function from Get_Start
-    j <- l_cur #also galling a Get_ClusterIndex function from Get_End
+  #l_cur=l_iter.First(net->link_list); ##From links of n_cur, first link in list is assigned as l_cur
+  
+  while (!iter.End()){ #Figure out how to code this into R, until the end of the iteration...
+    i <- Get_ClusterIndex(Get_Start(l_cur)) #grab cluster index for the starting node in l_cur's list
+    j <- Get_ClusterIndex(Get_End(l_cur)) #grab cluster index for the ending node in l_cur's list
     
-    Qmatrix[i,j] <- Qmatrix[i,j]+l_cur #also calling a Get_Weight function here
-    Qmatrix[j,i] <- Qmatrix[j,i]+l_cur #also calling a Get_Weight function here
+    Qmatrix[i,j] <- Qmatrix[i,j]+Get_Weight(l_cur) 
+    Qmatrix[j,i] <- Qmatrix[j,i]+Get_Weight(l_cur) 
     
-    l_cur <- l_iter[i+1]
+    #l_cur=l_iter.Next();
   }
   
   for (i in 0:q){
@@ -47,5 +51,5 @@ init_Qmat <- function(){
       Qa[i] <- Qa+Qmatrix[i,j]
     }
   }
-  calculate_Q
+  calculate_Q() #This is a function!
 }
