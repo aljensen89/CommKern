3d7a46c5548a0d9dd87e13edc84d9c8f49cfe47b
Alex Jensen <alexandria.jensen@ucdenver.edu>
2020-09-30 16:49:31 -0600

    Updated code to reflect changes for R

--- R/init_config.R
+++ R/init_config.R
@@ -18,54 +18,43 @@
 #' @export
 
 init_config <- function(spin) {
-  s < -0
   
-  #DLList_Iter<NNode*> iter; ##Creating an iteration using the NNode constructor, function holding node-based info
-  #DLList_Iter<NLink*> l_iter; ##Creating an iteration using the NLink constructor, function holding edge-based info
-  #NNode *n_cur; ##The current node being examined
-  #NLink *l_cur; ##The current link being examined
-  
-  sum_weight <-0
+  #Need to initialize color_filed elsewhere in code
+  ##color_field<-rep(0,q)
+  s <- 0
+  sum_weight <- 0
   av_k_squared <- 0
   av_k <- 0
-  
-  #Initialize color_field
-  for (i in 0:q){
-    color_field[i] = 0
-  }
-  
   total_degree_sum <- 0
   
-  #n_cur=iter.First(net->node_list); ##the first node in the network list is the current node
-  
-  while (!iter.End()){ #Figure out how to code this into R, until the end of the iteration...
-    ifelse(spin<0, s <- sample(1:q,1), s <- spin) #If the spin is soehow <0, randomly assign s to an integer, otherwise assign as normal
-    #n_cur->Set_ClusterIndex(s); ##
-    #l_cur=l_iter.First(n_cur->Get_Links()); ##From links of n_cur, first link in list is assigned as l_cur
+  for (i in 1:length(network$vertexes$node_id)){
+    #If the spin is somehow <0, randomly assign s to an integer, otherwise assign as normal
+    ifelse(spin<0,
+           s <- sample(1:q,1),
+           s <- spin) 
     
-    sum_weight <- 0
+    sum_weight <- network$vertexes$func_degree[i]
     
-    while(!l_iter.End()){ #Figure out how to code this into R, until the end of the link iteration...
-      sum_weight <- sum_weight + Get_Weight(l_cur) #weight should be one, in case we are not using it.
-      #l_cur=l_iter.Next(); ##Move to the next link in the node's link list, making it the current link
-      
-    }
-    ##We set the sum of the weights or the degreeas the weight of the node, this way we don't have to calculate it again
-    #n_cur->Set_Weight(sum_weight);
+    network$vertexes$community[i] <- s
+    
+    #l_iter <- network$func_edges %>% 
+    #  filter(network$func_edges$func_start_node==network$vertexes$node_id[i] | 
+    #           network$func_edges$func_end_node==network$vertexes$node_id[i])
+    
     av_k_squared <- av_k_squared + (sum_weight)^2
     av_k <- av_k + sum_weight
     
     #In case we want all links to contribute equally - parameter gamma is fixed
-    ifelse(operation_mode==0, color_field[s] = colorfield[s] + 1, 
-           color_field[s] = color_field[s] + sum_weight)
+    ifelse(operation_mode==0, color_field[i] <- color_field[i] + 1, 
+           color_field[i] <- color_field[i] + sum_weight)
     
     #Or in the case we want to use a weight of each link that is proportional to k_i*k_j
     total_degree_sum <- total_degree_sum + sum_weight
-    #n_cur=iter.Next(); ##Move to the next node in the network list, making it the current node
+    
   }
-  av_k_squared <- av_k_squared / length(node_list)
-  av_k <- av_k / length(node_list)
   
-  #return net -> node_list -> Size()
+  av_k_squared <- av_k_squared / length(network$vertexes$node_id)
+  av_k <- av_k / length(network$vertexes$node_id)
+  
+  return(length(network$vertexes$node_id))
 }
-  
