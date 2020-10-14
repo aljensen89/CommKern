915560639e6c101498304be2d657ea4a48e0b5a8
Alex Jensen <alexandria.jensen@ucdenver.edu>
2020-10-14 14:59:20 -0600

    Finished first round of debugging

--- R/heatbath_lookup.R
+++ R/heatbath_lookup.R
@@ -20,91 +20,83 @@
 #' @export
 
 heatbath_lookup <- function(gamma,prob,kT,max_sweeps) {
-  #DLList_Iter<NNode*> iter, net_iter
-  #DLList_Iter<NLink*> l_iter
-  #DLList_Iter<unsigned int*> i_iter, i_iter2
-  #NNode *node, *n_cur
-  #NLink *l_cur
+  new_spin <- 0.0 
+  spin_opt <- 0.0 
+  old_spin <- 0.0 
   
-  new_spin <- 0.0 #unsigned integer
-  spin_opt <- 0.0 #unsigned integer
-  old_spin <- 0.0 #unsigned integer
+  sweep <- 0.0 
+  max_q <- 0.0 
+  rn < -0.0 
   
-  sweep <- 0.0 #unsigned integer
-  max_q <- 0.0 #long
-  rn < -0.0 #long
+  w <- 0.0 
+  degree <- 0.0 
+  norm <- 0.0 
+  r <- 0.0 
+  beta <- 0.0 
+  minweight <- 0.0 
+  prefac <- 0.0 
+  norm <- 0
   
-  #unsigned long changes, problemcount
-  
-  w <- 0.0 #double
-  degree <- 0.0 #double
-  norm <- 0.0 #double
-  r <- 0.0 #double
-  beta <- 0.0 #double
-  minweight <- 0.0 #double
-  prefac <- 0.0 #double
-  
-  found <- FALSE #boolean
-  
- num_of_nodes <- 0.0 #long int
-  
+  found <- FALSE
   sweep <- 0.0
   changes <- 1.0
+  problemcount <- 0
   
-  num_of_nodes <- Size(node_list) #uses call num_of_nodes=net->node_list->Size()
+  #num_of_nodes <- length(network$vertexes$node_id)
   
   while(sweep<max_sweep){
     sweep <- sweep+1
     
     #Loop over all nodes in network
-    for (n in 0:(num_of_nodes-1)){
-      rn <- rn-1
+    for (n in 0:num_of_nodes){
+      rn <- -1
       
-      while(rn<0 | rn>(num_of-nodes-1)){
-        rn <- sample(0:(num_of_nodes-1),1)
+      while(rn<0 | rn>num_of_nodes){
+        rn <- sample(1:num_of_nodes,1)
+      }
+      
+      node <- network$vertexes$node_id[rn]
+      
+      #Initialize the neighbors and the weights
+      for(i in 0:q){
+        neighbors[i] <- 0
+        weights[i] <- 0
+      }
+      deg <- network$vertexes$func_degree[node]
         
-        node<-Get(rn) #uses the node=net->node_list->Get(rn)
+      #Loop over all links (=neighbors)
+      l_iter <- network$func_edges %>% 
+        filter(network$func_edges$func_start_node==network$vertexes$node_id[node] | 
+               network$func_edges$func_end_node==network$vertexes$node_id[node])
         
-        #Initialize the neighbors and the weights
-        problemcount <- 0
-        for(i in 0:q){
-          neighbors[i] <- 0
-          weights[i] <- 0
-        }
-        norm <- 0
-        degree <- Get_Weight(node)
-        
-        #Loop over all links (=neighbors)
-        #l_cur=l_iter.First(node->Get_Links())
-        
-        while(!net_iter[length(net_iter)]){
-          w <- Get_Weight(l_cur)
+      for (j in 1:nrow(l_iter)){
+        w <- l_iter$func_weight[j]
           
-          if(node==Get_Start(l_cur)){
-            n_cur<-Get_End(l_cur)
-          }
-          else{
-            n_cur<-Get_Start(l_cur)
-          }
+        #If node is the starting node for the current link, then n_cur becomes l_cur's ending node
+        #otherwise it becomes l_cur's starting node
+        if(node==l_iter$func_start_node[j]){
+          n_cur <- l_iter$func_end_node[j]
+        }else{
+          n_cur <- l_iter$func_start_node[j]
+        }
           
-          neighbours[Get_ClusterIndex(n_cur)] <- w+neighbours
-          #l_cur=l_iter.Next()
-        }
+        neighbours[network$vertexes$community[network$vertexes$node_id==n_cur]] <- w + neighbours[network$vertexes$community[network$vertexes$node_id==n_cur]]
+      }
+      
+      #Search optimal spin
+      old_spin <- network$vertexes$community[network$vertexes$node_id==node]
         
-        #Look for optimal spin
-        old_spin <- Get_ClusterIndex(node)
-        
-        if (operation_mode==0){
-          delta <- 1.0
-        }
-        else if (operation_mode==1){ #Newman modularity
-          prob <- degree/total_degree_sum
-          delta <- degree
-        }
+      if (operation_mode==0){
+        prefac <- 1.0
+        delta <- 1.0
+      }else if (operation_mode==1){ #Newman modularity
+        prefac <- 1.0
+        prob <- deg/total_degree_sum #Is total_degree_sum initialized somewhere else?
+        delta <- deg
       }
       
       spin_opt <- old_spin
-      beta <- 1/kT*prefac
+      beta <- (1/kT)*prefac
       minweight <- 0
       weights[old_spin] <- 0
       
@@ -117,76 +109,72 @@ heatbath_lookup <- function(gamma,prob,kT,max_sweeps) {
             minweight <- weights[spin]
           }
         }
+      }
+      
+      for(spin in 1:q){ #all possible new spins
+        weights[spin] <- weights[spin]-minweight
+        weights[spin] <- exp(-beta*weights[spin])
+        norm <- norm+weights[spin]
+      }
         
-        for(spin in 1:q){ #all possible new spins
-          weights[spin] <- weights[spin]-minweight
-          weights[spin] <- exp{-beta*weights[spin]}
-          norm <- norm+weights[spin]
+      #Choose a new spin
+      r <- runif(0,norm)
+      new_spin <- 1
+      found <- FALSE
+        
+      while(found==FALSE & new_spin<=q){
+        if(r<=weights[new_spin]){
+          spin_opt <- new_spin
+          found <- TRUE
+        }else{
+          r <- r-weights[new_spin]
         }
+        new_spin <- new_spin+1
+      }
         
-        #Choose a new spin
-        r <- runif(0,norm)
-        new_spin <- 1
-        found <- FALSE
+      if(found == FALSE){
+        problemcount <- problemcount+1
+      }
         
-        while(!found & new_spin<=q){
-          if(r<=weights[new_spin]){
-            spin_opt <- new_spin
-            found <- TRUE
-            break
-          }
-          else{
-            r <- r-weights[new_spin]
-          }
-          new_spin <- new_spin+1
-        }
-        
-        if(!found){
-          problemcount <- problemcount+1
-        }
-        
-        #Now set the new spin
-        if(new_spin!=old_spin){
-          changes <- changes+1
-          node<-Set_ClusterIndex(new_spin)
+      #Now set the new spin
+      if(new_spin!=old_spin){
+        changes <- changes+1
+        node<-Set_ClusterIndex(new_spin)
           
-          color_field[old_spin] <- color_field[old_spin]-delta
-          color_field[new_spin] <- color_field[new_spin]+delta
+        color_field[old_spin] <- color_field[old_spin]-delta
+        color_field[new_spin] <- color_field[new_spin]+delta
           
-          #Q matrix update - iteration over all neighbors
-          #l_cur=l_iter.First(node->Get_Links())
-          while(!l_iter[length(l_iter)]){
-            w <- Get_Weight(l_cur)
-            if(node==Get_Start(l_cur)){
-              n_cur <- Get_End(l_cur)
-            }
-            else {
-              n_cur <- Get_Start(l_cur)
-            }
-            Qmatrix[old_spin,Get_ClusterIndex(n_cur)] <- Qmatrix[old_spin,Get_ClusterIndex(n_cur)]-w
-            Qmatrix[new_spin,Get_ClusterIndex(n_cur)] <- Qmatrix[new_spin,Get_ClusterIndex(n_cur)]+w
-            Qmatrix[Get_ClusterIndex(n_cur),old_spin] <- Qmatrix[Get_ClusterIndex(n_cur),old_spin]-w
-            Qmatrix[Get_ClusterIndex(n_cur),new_spin] <- Qmatrix[Get_ClusterIndex(n_cur),new_spin]+w
+        #Q matrix update - iteration over all neighbors
+        for (j in 1:nrow(l_iter)){
+          w2 <- l_iter$func_weight[j]
             
-            Qa[old_spin] <- Qa[old_spin]-w
-            Qa[new_spin] <- Qa[new_spin]+w
-            
-            #l_cur=l_iter.Next()
+          #If node is the starting node for the current link, then n_cur becomes l_cur's ending node
+          #otherwise it becomes l_cur's starting node
+          if(node==l_iter$func_start_node[j]){
+            n_cur2 <- l_iter$func_end_node[j]
+          }else{
+            n_cur2 <- l_iter$func_start_node[j]
           }
-         }
+          Qmatrix[old_spin,network$vertexes$community[network$vertexes$node_id==n_cur2]] <- Qmatrix[old_spin,network$vertexes$community[network$vertexes$node_id==n_cur2]]-w2
+          Qmatrix[new_spin,network$vertexes$community[network$vertexes$node_id==n_cur2]] <- Qmatrix[new_spin,network$vertexes$community[network$vertexes$node_id==n_cur2]]+w2
+          Qmatrix[network$vertexes$community[network$vertexes$node_id==n_cur2],old_spin] <- Qmatrix[network$vertexes$community[network$vertexes$node_id==n_cur2],old_spin]-w2
+          Qmatrix[network$vertexes$community[network$vertexes$node_id==n_cur2],new_spin] <- Qmatrix[network$vertexes$community[network$vertexes$node_id==n_cur2],new_spin]+w2
+            
+          Qa[old_spin] <- Qa[old_spin]-w2
+          Qa[new_spin] <- Qa[new_spin]+w2
+        }
+       }
       }
     }
-      max_q <- 0
+    max_q <- 0
       
-      for(i in 1:q){
-        if(color_field[i]>max(q)){
-          max_q <- color_field[i]+0.5
-        }
-        
-        acceptance <- changes/num_of_nodes/sweep
-        return(acceptance)
+    for(i in 1:q){
+      if(color_field[i]>max(q)){
+        max_q <- color_field[i]+0.5
       }
-  }
+    }
+ acceptance <- changes/num_of_nodes/sweep
+ return(acceptance)
 }
     
       
