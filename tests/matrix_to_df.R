f4c3b12ad9e96bf9b9fdd85a7bb44f41174955eb
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2022-06-24 08:42:50 -0600

    add a test for matrix_to_df
    re #5

---
+++ tests/matrix_to_df.R
@@ -0,0 +1,11 @@
+library(spinglass)
+
+################################################################################
+# Test that the provided SBM_net data set regenerates itself
+net <- matrix_to_df(func_mat = SBM_net$func_mat, str_mat = SBM_net$str_mat)
+stopifnot(identical(net, SBM_net))
+
+################################################################################
+#                                 End of File                                  #
+################################################################################
+
