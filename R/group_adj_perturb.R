4eabef9629c2ecb42456509a1b4007ba79a358c9
Peter DeWitt <peter.dewitt@cuanschutz.edu>
2022-08-05 08:45:22 -0600

    correct tag to resolve missmatched brackets; correct function name for linking between man pages

--- R/group_adj_perturb.R
+++ R/group_adj_perturb.R
@@ -1,24 +1,23 @@
 #' Group adjacency matrices
-#' 
-#' Description of the simulated group adjacency matrices function.  
-#' 
+#'
+#' Description of the simulated group adjacency matrices function.
+#'
 #' This function takes the output from the \code{\link{group_network_perturb}} function, which is a list of
 #' dataframes summarizing each simulated network, and creates an array of adjacency matrices. These adjacency
-#' matrices can then be used as input to any community detection algorithm (such as \code{link\{hier_mult_spin}}).
-#' 
-#' The function returns an array of adjacency matrices of dimension (n_nets x n_nodes x n_nodes)
-#' 
+#' matrices can then be used as input to any community detection algorithm (such
+#' as the hierarchcial multimodal spinglass algorithm, \code{\link{hms}}).
+#'
 #' @param group_network_list the output from \code{\link{group_network_perturb}}, which is a list of dataframes
 #' detailing nodes, community assignments of each node, and edge weights between each dyad of nodes
 #' @param n_nets the number of networks simulated
 #' @param n_nodes the number of nodes in each simulated network (will be the same across all networks)
-#' 
-#' @return adj_array, an array of adjacency matrices of dimension (n_nets x n_nodes x n_nodes)
-#' 
-#' @seealso \code{\link{group_network_perturb}}
-#' 
+#'
+#' @return an array of adjacency matrices of dimension (n_nets x n_nodes x n_nodes)
+#'
+#' @seealso \code{\link{group_network_perturb}}, \code{\link{hms}}
+#'
 #' @export
-#' 
+#'
 group_adj_perturb <- function(group_network_list,n_nets,n_nodes){
   adj_array <- array(NA,c(n_nets,n_nodes,n_nodes))
   for (j in 1:n_nets){
@@ -27,7 +26,7 @@ group_adj_perturb <- function(group_network_list,n_nets,n_nodes){
     mat <- matrix(0,nrow=n_nodes,ncol=n_nodes,dimnames=list(node_names,node_names))
     mat[as.matrix(group_network_list[[j]][,c(1,2)])] <- group_network_list[[j]][,wgt_col_name]
     mat[as.matrix(group_network_list[[j]][,c(2,1)])] <- group_network_list[[j]][,wgt_col_name]
-    
+
     adj_array[j,,] <- mat
   }
   return(adj_array)
