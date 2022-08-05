1c85bb72e5af2ccaa4e62ae1b9d70ae18037c56e
Alex Jensen <alexandria.jensen@ucdenver.edu>
2022-08-04 18:06:03 -0600

    Changed name of input

--- R/group_adj_perturb.R
+++ R/group_adj_perturb.R
@@ -19,14 +19,14 @@
 #' 
 #' @export
 #' 
-group_adj_perturb <- function(group_network_perturb,n_nets,n_nodes){
+group_adj_perturb <- function(group_network_list,n_nets,n_nodes){
   adj_array <- array(NA,c(n_nets,n_nodes,n_nodes))
   for (j in 1:n_nets){
-    wgt_col_name <- colnames(group_network_perturb[[j]])[6]
-    node_names <- union(group_network_perturb[[j]]$Node_A,group_network_perturb[[j]]$Node_B)
+    wgt_col_name <- colnames(group_network_list[[j]])[6]
+    node_names <- union(group_network_list[[j]]$Node_A,group_network_list[[j]]$Node_B)
     mat <- matrix(0,nrow=n_nodes,ncol=n_nodes,dimnames=list(node_names,node_names))
-    mat[as.matrix(group_network_perturb[[j]][,c(1,2)])] <- group_network_perturb[[j]][,wgt_col_name]
-    mat[as.matrix(group_network_perturb[[j]][,c(2,1)])] <- group_network_perturb[[j]][,wgt_col_name]
+    mat[as.matrix(group_network_list[[j]][,c(1,2)])] <- group_network_list[[j]][,wgt_col_name]
+    mat[as.matrix(group_network_list[[j]][,c(2,1)])] <- group_network_list[[j]][,wgt_col_name]
     
     adj_array[j,,] <- mat
   }
